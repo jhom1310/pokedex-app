ec66aafcc1381c69bbdfde06deab4f38914d8089
Aleff Souza <aleffjonathan7@gmail.com>
2021-11-22 11:10:50 -0300

    fix: correção na stats bar

---
+++ lib/app/global/widgets/stat.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:pokedex/app/global/widgets/stats_bar.dart';
+
+class Stat extends StatelessWidget {
+  const Stat({
+    required this.label,
+    required this.value,
+  });
+
+  final String label;
+
+  final num value;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(5),
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        children: <Widget>[
+          Expanded(
+            flex: 1,
+            child: Text(
+              label,
+              style: TextStyle(color: Colors.black.withOpacity(0.6)),
+            ),
+          ),
+          Expanded(
+            flex: 5,
+            child: ProgressBar(
+              progress: value / 100,
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Text('$value'),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/app/modules/home/views/detail.dart
+++ lib/app/modules/home/views/detail.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:pokedex/app/data/models/pokemon_model.dart';
 import 'package:pokedex/app/database/storage.dart';
+import 'package:pokedex/app/global/widgets/stat.dart';
 import 'package:pokedex/app/global/widgets/type.dart';
-import 'package:percent_indicator/percent_indicator.dart';
 import 'package:pokedex/app/modules/home/controllers/home_controller.dart';
 
 class DetailView extends StatelessWidget {
@@ -104,29 +104,7 @@ class DetailView extends StatelessWidget {
             ),
             SizedBox(height: 15),
             for (var item in poke.stats)
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Container(
-                    //color: Colors.red,
-                    margin: EdgeInsets.all(5),
-                    child: Text(item.stat.name.toUpperCase()),
-                  ),
-                  SizedBox(width: 10),
-                  Container(
-                    alignment: Alignment.centerLeft,
-                    child: LinearPercentIndicator(
-                      width: Get.width * 0.50,
-                      lineHeight: 14.0,
-                      percent: 0.5,
-                      backgroundColor: Colors.grey[200],
-                      progressColor: Colors.yellow[700],
-                      //leading: Text(item.stat.name.toUpperCase()),
-                      trailing: Text(item.baseStat.toString()),
-                    ),
-                  ),
-                ],
-              ),
+              Stat(label: item.stat.name, value: item.baseStat),
             Container(
               margin: EdgeInsets.all(50),
               alignment: Alignment.center,
